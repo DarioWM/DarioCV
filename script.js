b955b8fef48f16b605e922371a84fd0861a64ef9
DarioWM <168138515+DarioWM@users.noreply.github.com>
2024-04-25 23:51:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// Función para mostrar el modal
+function mostrarTraduccion(idModal) {
+    var modal = document.getElementById(idModal);
+    modal.style.display = "block";
+
+    // Agregar evento de clic al fondo oscuro
+    modal.onclick = function(event) {
+        if (event.target == modal) {
+            cerrarModal(idModal);
+        }
+    };
+}
+
+// Función para cerrar el modal
+function cerrarModal(idModal) {
+    var modal = document.getElementById(idModal);
+    modal.style.display = "none";
+}
+
+// Función para alternar el zoom de la imagen
+function toggleZoom(imageId) {
+    var image = document.getElementById(imageId);
+    if (image.style.maxWidth === "70%") {
+        image.style.maxWidth = "30%"; // Vuelve al tamaño original del 30%
+    } else {
+        image.style.maxWidth = "70%"; // Ajusta al tamaño del 70%
+    }
+}
+
+function toggleContacto() {
+    var dropdown = document.getElementById("contacto-dropdown");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
