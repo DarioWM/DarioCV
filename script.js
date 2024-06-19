2188dd592d1f6a57418b0e13807d997d08e9e687
DarioWM <168138515+DarioWM@users.noreply.github.com>
2024-06-19 13:24:30 +0200

    mod2

--- script.js
+++ script.js
@@ -1,48 +1,210 @@
-// Función para mostrar el modal
-function mostrarTraduccion(idModal) {
-    var modal = document.getElementById(idModal);
-    modal.style.display = "block";
-
-    // Agregar evento de clic al fondo oscuro
-    modal.onclick = function(event) {
-        if (event.target == modal) {
-            cerrarModal(idModal);
-        }
-    };
+/* Estilos generales */
+body {
+    background-color: #ede9f3;
+    font-family: 'Ubuntu', sans-serif;
+    margin: 0;
+    padding: 0;
 }
 
-// Función para cerrar el modal
-function cerrarModal(idModal) {
-    var modal = document.getElementById(idModal);
-    modal.style.display = "none";
+h1, .main-title {
+    text-align: center;
+    color: #333;
 }
 
-// Función para alternar el zoom de la imagen
-function toggleZoom(imageId) {
-    var image = document.getElementById(imageId);
-    if (image.style.maxWidth === "70%") {
-        image.style.maxWidth = "30%"; // Vuelve al tamaño original del 30%
-    } else {
-        image.style.maxWidth = "70%"; // Ajusta al tamaño del 70%
+.diploma-container {
+    margin: 20px auto;
+    text-align: center;
+    margin-top: 40px;
+}
+
+.diploma {
+    max-width: 30%;
+    border: 10px solid #839fb1;
+    padding-left: 1%;
+    padding-right: 1%;
+    transition: max-width 0.3s ease-in-out;
+    cursor: pointer;
+}
+
+/* Estilos para el botón de traducción */
+.traduccion-btn {
+    background-color: #4c7faf;
+    border: none;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: block;
+    width: 31%;
+    margin: 10px auto;
+    font-size: 16px;
+    cursor: pointer;
+    margin-top: 1px;
+    animation: cambioColor 0.7s infinite alternate; /* Agregamos la animación */
+}
+
+@keyframes cambioColor {
+    0% {
+        background-color: #4c7aaf; /* Color inicial */
+    }
+    50% {
+        background-color: #4c66af; /* Color intermedio */
+    }
+    100% {
+        background-color: #6a4caf; /* Color final */
     }
 }
 
-// Función para mostrar y ocultar el menú de contacto
-function toggleContacto() {
-    var dropdown = document.getElementById("contacto-dropdown");
-    if (dropdown.style.display === "block") {
-        dropdown.style.display = "none";
-    } else {
-        dropdown.style.display = "block";
+/* Estilos para modal */
+.modal {
+    display: none;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgb(0,0,0);
+    background-color: rgba(0,0,0,0.4);
+}
+
+.modal-content {
+    background-color: #fefefe;
+    margin: 10% auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+}
+
+.close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+/* Estilos para pantalla pequeña */
+@media screen and (max-width: 600px) {
+    .diploma {
+        max-width: 70%;
+    }
+
+    .main-title {
+        font-size: 30px;
+    }
+
+    .texto {
+        font-size: 20px;
+    }
+
+    .contacto-btn {
+        font-size: 20px;
+    }
+
+    .navbar-header h1 {
+        font-size: 30px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        width: 100%;
     }
 }
 
-// Cerrar el menú de contacto al hacer clic fuera de él
-window.onclick = function(event) {
-    var dropdown = document.getElementById("contacto-dropdown");
-    if (!event.target.matches('.contacto-btn')) {
-        if (dropdown.style.display === "block") {
-            dropdown.style.display = "none";
-        }
+/* Estilos para pantalla muy pequeña */
+@media screen and (max-width: 400px) {
+    .diploma {
+        max-width: 90%;
     }
+
+    .main-title {
+        font-size: 25px;
+    }
+
+    .texto {
+        font-size: 18px;
+    }
+
+    .contacto-btn {
+        font-size: 18px;
+    }
+
+    .navbar-header h1 {
+        font-size: 25px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        width: 100%;
+    }
+}
+
+.navbar {
+    background-color: #28374b;
+    overflow: hidden;
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.navbar-header h1 {
+    color: white;
+    margin: 0;
+    font-size: 50px;
+    font-weight: normal;
+    text-align: center;
+    white-space: nowrap; /* Evita el salto de línea */
+    overflow: hidden; /* Oculta el texto que se sale del contenedor */
+    text-overflow: ellipsis; /* Añade puntos suspensivos (...) si el texto es demasiado largo */
+    width: 100%;
+}
+
+.contacto-btn {
+    background-color: #4c9baf;
+    border: none;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 30px;
+    margin-top: 10px;
+    cursor: pointer;
+}
+
+/* Estilos para el menú desplegable de información de contacto */
+.contacto-dropdown {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 200px; /* Ancho mínimo para asegurar que no se corte */
+    box-shadow: 0px 8px 16px 0px rgba(42, 71, 116, 0.2);
+    padding: 12px 16px;
+    z-index: 1;
+}
+
+/* Estilos para el menú desplegable de información de contacto en dispositivos pequeños */
+@media screen and (max-width: 600px) {
+    .navbar {
+        position: relative;
+    }
+    .contacto-dropdown {
+        position: absolute;
+        top: 50px;
+        right: 0;
+        min-width: 200px; /* Ancho mínimo para asegurar que no se corte */
+        max-width: 100%;
+        box-sizing: border-box;
+    }
+}
+
+.texto {
+    text-align: center;
 }
